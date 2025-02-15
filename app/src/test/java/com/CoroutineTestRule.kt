e360eb8ad7ffa6c33e2fc2ea8c61b5dd000c4499
EladJarby <eladjarby@gmail.com>
2025-02-16 00:16:39 +0200

    Fix coroutine test rule

--- app/src/test/java/com/CoroutineTestRule.kt
+++ app/src/test/java/com/CoroutineTestRule.kt
@@ -2,11 +2,10 @@ package com
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestDispatcher
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.setMain
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
